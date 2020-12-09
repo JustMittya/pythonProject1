dc9d0cb876b7753933930846cb36b92eba6d9e6a
JustMittya <37212337+JustMittya@users.noreply.github.com>
2020-12-09 11:21:00 +0300

    firstless

---
+++ 1.py
@@ -0,0 +1,8 @@
+question1 = 'Висит груша, нельзя скушать'
+answer = 'Груша'
+print(question1)
+answer_text = input ('Ваш ответ?')
+if answer_text == answer:
+    print ('Молодец, правильно')
+else:
+    print('Попробуй еще раз')
---
+++ main.py
@@ -0,0 +1,27 @@
+class Car:
+    pass
+
+c = Car()
+print(c,type(C))
+
+class Room:
+    number = 'Room34'
+    floor = 4
+
+r = Room()
+r1 = Room()
+print(r.number, r1.number)
+print(r.floor, r1.floor)
+
+class Door:
+    def open(self):
+        print('self is', self)
+        print('Door is opened!')
+        self.opened = True
+
+d = door()
+d.open()
+
+d1 = Door ()
+d = open()
+
